9874205d231bd14a5b73a37058be253f186bfbce
B10615035 <B10615035@mail.ntust.edu.tw>
2021-04-27 16:58:43 +0800

    update company name

--- src/utils/companyIndex.js
+++ src/utils/companyIndex.js
@@ -1,4 +1,4 @@
-const companyList = ['中華電信', '台達電子', '邑富', '利凌企業', '英業達', '研揚科技', '鈊象電子', '緯創資通']
+const companyList = ['中華電信', '台達電子工業', '邑富', '利凌企業', '英業達', '研揚科技', '鈊象電子', '緯創資通']
 
 const companyName = (index) => {
     return companyList[index]
