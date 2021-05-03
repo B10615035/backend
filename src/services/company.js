15c03bcbe8be5557c0068c6a73aabf8008a61bad
B10615035 <B10615035@mail.ntust.edu.tw>
2021-05-03 12:56:21 +0800

    update company findOne sort

--- src/services/company.js
+++ src/services/company.js
@@ -1,7 +1,9 @@
 import HttpStatus from '../utils/HttpStatus'
 import companyModel from '../models/company'
 import studentModel from '../models/student'
-import {companyName} from '../utils/companyIndex'
+import {
+    companyName
+} from '../utils/companyIndex'
 
 class comapnyService {
     constructor() {
@@ -15,30 +17,52 @@ class comapnyService {
         })
         if (findID)
             return new HttpStatus(400, 'This company already exits.')
-        await this.company.create({...data, created_at: Date.now(), updated_at: Date.now()})
+        await this.company.create({
+            ...data,
+            created_at: Date.now(),
+            updated_at: Date.now()
+        })
         return new HttpStatus(200, 'Create company success.')
     }
 
-    async findAll(){
+    async findAll() {
         const company = await this.company.find()
         return new HttpStatus(200, company)
     }
 
-    async findOne(data){
+    async findOne(data) {
         const name = companyName(data.id)
-        const findCompany = await this.company.findOne({name: name})
-        if(!findCompany)
+        const findCompany = await this.company.findOne({
+            name: name
+        })
+        if (!findCompany)
             return new HttpStatus(400, 'This company is not exits.')
 
         const studentList = await this.student.find()
-        return new HttpStatus(200, {findCompany, studentList})
+
+        studentList = studentList.sort((a, b) => {
+            if (a.id > b.id)
+                return -1
+            else
+                return 0
+        })
+
+        return new HttpStatus(200, {
+            findCompany,
+            studentList
+        })
     }
 
-    async update(id, data){
+    async update(id, data) {
         const name = companyName(id.id)
         console.log(name)
-        const findID = await this.company.findOneAndUpdate({name: name},{...data, updated_at:Date.now()})
-        if(!findID)
+        const findID = await this.company.findOneAndUpdate({
+            name: name
+        }, {
+            ...data,
+            updated_at: Date.now()
+        })
+        if (!findID)
             return new HttpStatus(400, 'This company dose not exits.')
         return new HttpStatus(200, '提交成功')
     }
